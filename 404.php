048a3c9f6662c3c4f6adf9f72e90e8b2489fa650
Sean Batchelor <sean.carcass@gmail.com>
2012-05-09 08:19:38 +1000

    Revert "Revert "The Full Theme""
    This reverts commit a69613e0441d50a6233da966cd7c5ab4740d4945.

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+	<div role="main" class="row">
+		
+			<?php get_sidebar(); ?>
+
+		
+		<div class="nine columns">
+			<h2 class="center">Error 404! Not Found. This link is broke.</h2>
+			<p>
+				<img src="http://3.bp.blogspot.com/-yIT4jOt9DPY/Ta74YwFQLDI/AAAAAAAAAPE/9bxuiugjNfA/s1600/broke-mr_-monopoly-guy.jpg" />
+			</p>
+		</div>
+	</div>
+
+
+<?php get_footer(); ?>
